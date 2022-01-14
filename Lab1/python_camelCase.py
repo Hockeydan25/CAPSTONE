12988bffebf5101b020a6d51ebac3e4f7936d8d6
Dan Smestad <hockeydan25@gmail.com>
2022-01-14 13:25:30 -0600

    finished the code for camel_case program. Added in Quiz, i'll probably delete it and create a preoject1 folder for it instead.

--- Lab1/python_camelCase.py
+++ Lab1/python_camelCase.py
@@ -1,9 +1,40 @@
+#Lab1 Dan Smestad ITEC 2905-80 Software Dev. Capstone Clara James
 # Write a program that turns a sentence into camel case. 
 # The first word is lowercase, the rest of the words have their initial letter capitalized, 
 # and all of the words are joined together. For example, with the input "fOnt proCESSOR and ParsER", 
 # your program will output "fontProcessorAndParser". 
-#Lab1 Dan Smestad ITEC 2905-80 Software Dev. Capstone Clara James
 
-'fOnt proCESSOR and ParsER'
+output = '' #empty varible string we can put the new string in for storing. 
+
+edit = 'fOnt proCESSOR and ParsER' # string we want to edit.
+
+words = edit.split() # function split will seperate the string into a manageablbe list.
+# print(words)
+
+for i in range(len(words)): # setup for loop in range , using the length of the words to set loop count. 
+    if i == 0: # set i to zero first word we will apply a lower casing to it. 
+        output = words[i].lower() # takes our emtpy variable ans put the lowercase word font into.
+    else:                         # when not zero we will count trough 1-3 and capitalize each word.  
+        output = output + words[i].capitalize() #placeing words with function Cap to each and adding first output.
+print(output)         
+# print statement prints the final version. I would have thouth I needed to to use
+#  end= '' to remove new line? but it plsaced and words on one line. The loop probably did this inplace of the print statement where it would be new lines.
+
+
+
+#video code following along. 
+# class_code = { 2950: 'cap', 2905: 'physical', 3300: 'resumes', 3142: 'Literacy'}
+
+# for code in class_code:
+#     print(code)
+#     print(class_code[code])
+
+# for code, name in class_code.items(): # tuple I need help with tuple understaning.
+#     print('The class code is ' + str(code) + 'and the name is ' + name) 
+
+# for code, name in class_code.items(): # tuple I need help with tuple understaning.
+#     print(f'The class code is {code} and the name is {name}') # f  format string
+
+
 
 
--- Lab1/semester2022_classes.py
+++ Lab1/semester2022_classes.py
@@ -3,17 +3,17 @@
 # Save these class names in a list. while loop
 # Print all the items in the list, one per line.
 
-classes = []
-class_number = int(input('How Many classes are you taking this semester? '))
+classes = [] #empty list here to store class names. Array
+class_number = int(input('How Many classes are you taking this semester? ')) #user prompt to enter how many classes we aren't using int. 
 
-while True:
-    class_name = input('Enter the name of the class, or enter to quit: ') 
-    if not class_name:
-        break
-    classes.append(class_name)    
+while True: # while loop, user exits with enter key breaks loop
+    class_name = input('Enter the name of the class, or enter to quit: ') #prompt to enter class names
+    if not class_name: # if name is Ture prompt is repeated.
+        break # enter is keyed and loop exits.
+    classes.append(class_name)#calling the list of names user enters,we created empty list/array to start.   
 
 for x in classes: # prints out the each class name
-    print(x)
+    print(x) # x is printed variable name chosen to rep the class list names. Much more could be done to diplay.
 
 
 
